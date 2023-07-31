d6eff32bcb1dca84236c4fa92b9678a30e460462
Ayush <aayushtiwari071@gmail.com>
2023-07-31 14:31:45 +0530

    optimised framer

--- src/App.js
+++ src/App.js
@@ -1,97 +1,30 @@
 import Spline from '@splinetool/react-spline';
-import { useState } from 'react';
-import { IoMenu, IoLogoGithub, IoLogoLinkedin,  IoAtCircleOutline, IoArrowDownCircleOutline } from 'react-icons/io5'
-
+import { IoLogoGithub, IoLogoLinkedin, IoAtCircleOutline } from 'react-icons/io5'
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import 'swiper/css';
-
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
-
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import { Experience } from './data';
 import Project from './proect';
 import About from './about';
-
-
+import Navbar from './Navbar';
 
 function App() {
 
-  const [isActive, setIsActive] = useState(false)
-  const [isLoading, setIsLoading] = useState(false);
-
-  const handleDownloadResume = () => {
-    setIsLoading(true);
-
-    setTimeout(() => {
-      setIsLoading(false);
-
-    }, 3000);
-  };
   return (
 
     <AnimatePresence initial={false}>
       <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
-        {/*Navigation bar*/}
-        <nav className='w-full px-6 z-50 fixed inset-x-0  top-2 flex justify-center items-center'>
-          <div className='w-full p-4 md:w-880 bg-navBar rounded-2xl flex items-center'>
-            <p className='text-lg text-slate-200 cursor-pointer'><b>Ayush Tiwari</b></p>
-            <div className='hidden md:flex items-center gap-9 ml-12 flex-1'>
-              <a href='#home' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Home</a>
-              <a href='#about' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> About</a>
-              <a href='#projects' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Projects</a>
-              <a href='#Contact' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Contact me</a>
-              <button
-                onClick={handleDownloadResume}
-                className="ml-auto text-base text-slate-300 font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
-              >
-                <a href='https://drive.google.com/uc?export=download&id=1iwnBKJoSq0-9-CUg5oJqGBXR6IcOPRs8' className="flex items-center gap-2">
-                  <IoArrowDownCircleOutline className="h-6 w-6" />
-                  <span>Resume</span>
-                </a>
-                {isLoading && (
-                  <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
-                )}
-              </button>
-            </div>
 
-            <motion.div
-              whileTap={{ scale: 0.7 }}
-              className='block md:hidden ml-auto cursor-pointer' onClick={() => setIsActive(!isActive)}>
-              <IoMenu className='text-2xl text-textBase cursor-pointer' />
-            </motion.div>
-            {isActive && (
-              <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1.1 }}
-                exit={{ opacity: 0, scale: 0.5 }}
-                transition={{ delay: 0.1, type: 'tween' }}
-                className='p-4 w-275 bg-navBar rounded-lg fixed top-24 right-10 flex flex-col items-center justify-evenly gap-5'>
-                <a href='#home' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Home</a>
-                <a href='#about' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> About</a>
-                <a href='#projects' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Projects</a>
-                <a href='#Contact' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Contact me</a>
-                <button
-                  onClick={handleDownloadResume}
-                  className="text-base text-slate-300 font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
-                >
-                  <a href='https://drive.google.com/uc?export=download&id=1iwnBKJoSq0-9-CUg5oJqGBXR6IcOPRs8' className="flex items-center gap-2">
-                    <IoArrowDownCircleOutline className="h-6 w-6" />
-                    <span>Resume</span>
-                  </a>
-                  {isLoading && (
-                    <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
-                  )}
-                </button>
-              </motion.div>
-            )}
-          </div>
-        </nav>
+        {/*Navigation bar*/}
+        <Navbar />
+
         <div className="relative h-screen w-full" id="home">
           <Spline scene="https://prod.spline.design/2XATKb6a1CPylYeF/scene.splinecode" />
           <div className='absolute bottom-5 w-full justify-center items-center flex'>
@@ -105,9 +38,7 @@ function App() {
         {/*Main section */}
         <main className='w-[80%] mt-11'>
           {/*about section*/}
-          <About/>
-          
-
+          <About />
           {/*Timeline Section*/}
           <section className='w-full flex items-center justify-center'>
             <VerticalTimeline>
@@ -131,31 +62,31 @@ function App() {
                 ))
               }
             </VerticalTimeline>
-            </section>
-            {/*Project section*/}
-            <Project />
-            {/* contact section  */}
-            <section id='Contact' className='flex flex-col items-center justify-center w-full my-24'>
-              <p className='text-2xl text-gray-300 capitalize'> <b>Contact me</b> </p>
-              {/* linkedin  */}
-              <div className='flex items-center justify-center w-full my-4 flex-wrap gap-4'>
-                <motion.a
-                  whileTap={{ scale: 0.8 }}
-                  href='https://www.linkedin.com/in/ayush-tiwari-538ab824b/' className='w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3'>
-                  <IoLogoLinkedin className='text-blue-700 text-3xl cursor-pointer' />
-                  <p className='text-lg text-textBase'>Linkedin</p>
-                </motion.a>
-              </div>
-              {/* github */}
-              <div className='flex items-center justify-center w-full my-4 flex-wrap gap-4'>
-                <motion.a
-                  whileTap={{ scale: 0.8 }}
-                  href='https://github.com/aaayushh7' className='w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3'>
-                  <IoLogoGithub className='text-textBase text-3xl cursor-pointer' />
-                  <p className='text-lg text-textBase'>Github</p>
-                </motion.a>
-              </div>
-            </section>
+          </section>
+          {/*Project section*/}
+          <Project />
+          {/* contact section  */}
+          <section id='Contact' className='flex flex-col items-center justify-center w-full my-24'>
+            <p className='text-2xl text-gray-300 capitalize'> <b>Contact me</b> </p>
+            {/* linkedin  */}
+            <div className='flex items-center justify-center w-full my-4 flex-wrap gap-4'>
+              <motion.a
+                whileTap={{ scale: 0.8 }}
+                href='https://www.linkedin.com/in/ayush-tiwari-538ab824b/' className='w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3'>
+                <IoLogoLinkedin className='text-blue-700 text-3xl cursor-pointer' />
+                <p className='text-lg text-textBase'>Linkedin</p>
+              </motion.a>
+            </div>
+            {/* github */}
+            <div className='flex items-center justify-center w-full my-4 flex-wrap gap-4'>
+              <motion.a
+                whileTap={{ scale: 0.8 }}
+                href='https://github.com/aaayushh7' className='w-full md:w-auto px-3 md:px-8 py-5 border border-zinc-800 rounded-2xl hover:border-zinc-600 duration-100 ease-in-out cursor-pointer flex items-center justify-center gap-3'>
+                <IoLogoGithub className='text-textBase text-3xl cursor-pointer' />
+                <p className='text-lg text-textBase'>Github</p>
+              </motion.a>
+            </div>
+          </section>
         </main>
       </div>
     </AnimatePresence>
---
+++ src/Navbar.js
@@ -0,0 +1,78 @@
+import { IoMenu,IoArrowDownCircleOutline} from 'react-icons/io5'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+
+const Navbar = () => {
+
+  const [isActive, setIsActive] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDownloadResume = () => {
+    setIsLoading(true);
+
+    setTimeout(() => {
+      setIsLoading(false);
+
+    }, 3000);
+  };
+    return (
+        
+        <nav className='w-full px-6 z-50 fixed inset-x-0  top-2 flex justify-center items-center'>
+          <div className='w-full p-4 md:w-880 bg-navBar rounded-2xl flex items-center'>
+            <p className='text-lg text-slate-200 cursor-pointer'><b>Ayush Tiwari</b></p>
+            <div className='hidden md:flex items-center gap-9 ml-12 flex-1'>
+              <a href='#home' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Home</a>
+              <a href='#about' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> About</a>
+              <a href='#projects' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Projects</a>
+              <a href='#Contact' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out'> Contact me</a>
+              <button
+                onClick={handleDownloadResume}
+                className="ml-auto text-base text-slate-300 font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
+              >
+                <a href='https://drive.google.com/uc?export=download&id=1iwnBKJoSq0-9-CUg5oJqGBXR6IcOPRs8' className="flex items-center gap-2">
+                  <IoArrowDownCircleOutline className="h-6 w-6" />
+                  <span>Resume</span>
+                </a>
+                {isLoading && (
+                  <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
+                )}
+              </button>
+            </div>
+
+            <motion.div
+              whileTap={{ scale: 0.7 }}
+              className='block md:hidden ml-auto cursor-pointer' onClick={() => setIsActive(!isActive)}>
+              <IoMenu className='text-2xl text-textBase cursor-pointer' />
+            </motion.div>
+            {isActive && (
+              <motion.div
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1.1 }}
+                exit={{ opacity: 0, scale: 0.5 }}
+                transition={{ delay: 0.1, type: 'tween' }}
+                className='p-4 w-275 bg-navBar rounded-lg fixed top-24 right-10 flex flex-col items-center justify-evenly gap-5'>
+                <a href='#home' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Home</a>
+                <a href='#about' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> About</a>
+                <a href='#projects' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Projects</a>
+                <a href='#Contact' className='text-base text-textBase font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Contact me</a>
+                <button
+                  onClick={handleDownloadResume}
+                  className="text-base text-slate-300 font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
+                >
+                  <a href='https://drive.google.com/uc?export=download&id=1iwnBKJoSq0-9-CUg5oJqGBXR6IcOPRs8' className="flex items-center gap-2">
+                    <IoArrowDownCircleOutline className="h-6 w-6" />
+                    <span>Resume</span>
+                  </a>
+                  {isLoading && (
+                    <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
+                  )}
+                </button>
+              </motion.div>
+            )}
+          </div>
+        </nav>
+        );
+    };
+  
+    export default Navbar;
