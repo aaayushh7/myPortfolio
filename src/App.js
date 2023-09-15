5a56afafcd0a96e180ac96b305f84f2c8eb124a1
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-09-16 03:18:46 +0530

    app update

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Plinefile from './spline';
 function App() {
 
   return (
-
     <AnimatePresence initial={false}>
       <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
         {/*Navigation bar*/}
