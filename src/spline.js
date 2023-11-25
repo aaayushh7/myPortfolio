2522373e5eb4d4574b9c45388eda1bd6adee9807
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-11-25 17:33:29 +0530

    DeskLoader

--- src/spline.js
+++ src/spline.js
@@ -1,19 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
-import {  IoAtCircleOutline } from 'react-icons/io5'
+import { IoAtCircleOutline } from 'react-icons/io5';
 
 
-export default function spline() {
+export default function SplineScene() {
+  const [loading, setLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setProgress(prevProgress => {
+        if (prevProgress === 100) {
+          setLoading(false);
+          clearInterval(timer);
+          return prevProgress;
+        }
+        const diff = Math.random() * 0.5 + 0.5;
+        const newProgress = Number((prevProgress + diff).toFixed(2));
+        return Math.min(newProgress, 100);
+      });
+    }, 100);
+
+    return () => clearInterval(timer);
+  }, []);
+
   return (
-    <div className="relative h-screen w-[99%]" id='home'>
+
+    <div className="relative h-screen w-[99%]">
+      {loading ? (
+        <div className="absolute inset-0 z-10 flex items-center justify-center bg-black bg-opacity-40">
+          <div className="text-lg text-white">
+            <div className="animate-pulse text-center">
+              <div className="text-md font-bold text-textBase pb-1">
+                Desk Loading {progress.toFixed(1)}%
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <div className="w-11 h-11 rounded-full animate-pulse flex items-center justify-center">
+                <div className="w-5 h-5 bg-icodow rounded-full animate-spin"></div>
+              </div>
+              </div>
+
+          </div>
+        </div>
+      ) : (
+        <>
           <Spline scene="https://prod.spline.design/2XATKb6a1CPylYeF/scene.splinecode" />
           <div className='absolute bottom-5 w-full justify-center items-center flex'>
             <div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl cursor-pointer'>
-              <IoAtCircleOutline className='text-md text-textBase'>
-              </IoAtCircleOutline>
+              <IoAtCircleOutline className='text-md text-textBase'></IoAtCircleOutline>
               <p className='text-md text-textBase'>ayushtiwari.co</p>
             </div>
           </div>
-        </div>
-  )
+        </>
+      )}
+
+    </div>
+  );
 }
+
+
+
+
+
+
+// import React from 'react'
+// import Spline from '@splinetool/react-spline';
+// import {  IoAtCircleOutline } from 'react-icons/io5'
+
+
+// export default function spline() {
+//   return (
+//     <div className="relative h-screen w-[99%]" id='home'>
+//           <Spline scene="https://prod.spline.design/2XATKb6a1CPylYeF/scene.splinecode" />
+//           <div className='absolute bottom-5 w-full justify-center items-center flex'>
+//             <div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl cursor-pointer'>
+//               <IoAtCircleOutline className='text-md text-textBase'>
+//               </IoAtCircleOutline>
+//               <p className='text-md text-textBase'>ayushtiwari.co</p>
+//             </div>
+//           </div>
+//         </div>
+//   )
+// }
