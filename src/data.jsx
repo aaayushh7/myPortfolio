4daf85ec7369e60baa6d48e3fedd994e26af77dc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-28 03:14:34 +0530

    vite config

---
+++ src/data.jsx
@@ -0,0 +1,52 @@
+import {IoFastFoodOutline,IoLogoAndroid,IoNuclearOutline,IoLogoTux,IoCodeSlash,IoGitBranch} from 'react-icons/io5'
+
+export const Experience = [
+    {
+        id: 1,
+        date: "Nov' 22",
+        iconsSrc: <IoFastFoodOutline />,
+        title: "WebDev",
+        location: "React,Next,Go",
+        description: "Go tools for new developers to [verify emails(spf,dmarc etc), chatgpt for terminal, sentence completion] and much more in Github"
+    },
+    {
+        id: 2,
+        date: "Mar' 23",
+        iconsSrc: <IoLogoAndroid />,
+        title: "AppDev",
+        location: "Women Safety",
+        description: "A women's safety app that sends instant SOS messages, including precise location, to trusted contact, ensuring swift help during emergencies"
+    }, {
+        id: 3,
+        date: "Dec'2022",
+        iconsSrc: <IoNuclearOutline />,
+        title: "Machine Learning",
+        location: "Anaconda, Jupyter",
+        description: "Regression, Classification and clustering Algorithms, Familiar with Lib: TensorFlow, scikit-learn OpenCV -{'>'} Disaster Prediction, weather Prediction, Car NumberPlate Recognition system"
+    },
+    {
+        id: 4,
+        date: "apr'19 - feb'22",
+        iconsSrc: <IoLogoTux />,
+        title: "Linux",
+        location: "Ubuntu, Fedora",
+        description: "Proficient in Linux OS, Package Management, Shell scripting, Proficient in linux utilities, Updated with linux developments through professional resources"
+
+    },
+    {
+        id: 5,
+        date: "Apr'22-December'22",
+        iconsSrc: <IoCodeSlash />,
+        title: "Java/C/Cpp/Python/JS/TS",
+        location: "Resources",
+        description: "Object-Oriented, Algorithms, Databases, Libraries"
+    },
+    {
+        id: 6,
+        date: "2023",
+        iconsSrc: <IoGitBranch />,
+        title: "Loading...",
+        location: "Enhancing Skills",
+        description: " Actively acquiring proficiency in emerging technologies.(Kubernetes, Go)"
+    },
+]
