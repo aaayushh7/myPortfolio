fd2be5747a5376f021f893e884bcb7b5c33193dc
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2023-12-28 03:16:55 +0530

    Loading time 1/8, smoothness 2x

--- src/spline.jsx
+++ src/spline.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import Spline from '@splinetool/react-spline';
+import React, { Suspense, useState, useEffect } from 'react';
 import { IoAtCircleOutline } from 'react-icons/io5';
 
+const Spline = React.lazy(() => import('@splinetool/react-spline'));
 
 export default function SplineScene() {
   const [loading, setLoading] = useState(true);
@@ -28,38 +28,43 @@ export default function SplineScene() {
   const splineSceneURL = "https://prod.spline.design/2XATKb6a1CPylYeF/scene.splinecode";
 
   return (
-    
-      <section className="relative h-screen w-[99%]" id='home'>
-        {loading ? (
-          <div className="absolute z-10 flex items-center justify-center bg-black bg-opacity-20 w-full h-full">
-            <div className="text-lg text-white">
-              <div className="text-center">
-                <div className="text-md font-bold text-textBase pb-1">
-                  Desk Loading {progress.toFixed(1)}%
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <div className="w-11 h-11 rounded-full animate-pulse flex items-center justify-center">
-                  <div className="w-5 h-5 bg-icodow rounded-full animate-spin"></div>
-                </div>
-              </div>
 
+    <section className="relative h-screen w-[99%]" id='home'>
+      {loading ? (
+        <div className="absolute z-10 flex items-center justify-center bg-black bg-opacity-20 w-full h-full">
+          <div className="text-lg text-white">
+            <div className="text-center">
+              <div className="text-md font-bold text-textBase pb-1">
+                Desk Loading {progress.toFixed(1)}%
+              </div>
+            </div>
+            <div className="flex justify-center">
+              <div className="w-11 h-11 rounded-full animate-pulse flex items-center justify-center">
+                <div className="w-5 h-5 bg-icodow rounded-full animate-spin"></div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      ) : (
+        <>
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <Spline scene={splineSceneURL} />
+          </Suspense>
+
+          <div className='absolute bottom-5 w-full justify-center items-center flex'>
+            <div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl cursor-pointer'>
+              <IoAtCircleOutline className='text-md text-textBase'></IoAtCircleOutline>
+              <p className='text-md text-textBase'>ayushtiwari.co</p>
             </div>
           </div>
-        ) : (
-          <>
-            <Spline scene={splineSceneURL} />
+        </>
+      )}
 
-            <div className='absolute bottom-5 w-full justify-center items-center flex'>
-              <div className='shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl cursor-pointer'>
-                <IoAtCircleOutline className='text-md text-textBase'></IoAtCircleOutline>
-                <p className='text-md text-textBase'>ayushtiwari.co</p>
-              </div>
-            </div>
-          </>
-        )}
 
-      </section>
+
+    </section>
   );
 }
 
---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react";
+import svgrPlugin from "vite-plugin-svgr";
+import tailwindcss from "tailwindcss";
+
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // This changes the out put dir from dist to build
+  // comment this out if that isn't relevant for your project
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  build: {
+    outDir: "build",
+  },
+  plugins: [
+    reactRefresh(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+        // ...svgr options (https://react-svgr.com/docs/options/)
+      },
+    }),
+  ],
+});
