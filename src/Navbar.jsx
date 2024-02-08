03988ca9aae3e93d08f3ebb635770a3a98ea3e11
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-08 18:32:46 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,7 +2,7 @@ import { IoMenu, IoArrowDownCircleOutline, IoEyeOutline } from 'react-icons/io5'
 import { useState } from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import React from 'react';
-import {Link} from "react-scroll";
+import { Link } from "react-scroll";
 
 
 const Navbar = React.memo(() => {
@@ -52,12 +52,12 @@ const Navbar = React.memo(() => {
                             )}
                         </button>
                         <button
-                        onClick={handleDownloadResume2}
+                            onClick={handleDownloadResume2}
                             className="ml-auto text-base hover:text-[#262627] font-medium text-slate-100 cursor-pointer duration-500 ease-in-out bg-icodow hover:bg-inherit shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-2 py-1 rounded-3xl border border-icodow"
                         >
                             <a href='/resume-ayush.pdf' target='_blank' className="flex items-center">
                                 <IoEyeOutline className="h-6 w-6" />
-                                
+
                             </a>
                             {isLoading2 && (
                                 <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
@@ -70,52 +70,52 @@ const Navbar = React.memo(() => {
                         className='block md:hidden ml-auto cursor-pointer' onClick={() => setIsActive(!isActive)}>
                         <IoMenu className='text-2xl text-[#262627] cursor-pointer' />
                     </motion.div>
-                    
+
                     <AnimatePresence>
-                    {isActive && (
-                        <motion.div
-                            key="mobile-menu"
-                            initial={{ opacity: 0, scale: 0.5 }}
-                            animate={{ opacity: 1, scale: 1 }}
-                            exit={{ opacity: 0, scale: 0.3 }}
-                            transition={{ delay: 0.1, type: 'spring' }}
-                            className='p-4 w-275 bg-[#a8b9ee] bg-opacity-70 backdrop-blur-md drop-shadow-lg rounded-lg fixed top-24 right-10 flex flex-col items-center justify-evenly gap-5'>
-                            <Link to="home" spy={true} smooth={true} offset={50} duration={2000} className='text-base  text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Home</Link>
-                            <Link to="about" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> About</Link>
-                            <Link to="timeline" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Timeline</Link>
-                            <Link to="projects" spy={true} smooth={true} offset={0} duration={1500} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Projects</Link>
-                            <Link to="Contact" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Contact me</Link>
-                            <button
-                                onClick={handleDownloadResume}
-                                className="text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
-                            >
-                                <a href='https://drive.google.com/uc?export=download&id=1_cwvSpfDsWjm6ooX9T2EO99zgeigaLrf' className="flex items-center gap-2">
-                                    <IoArrowDownCircleOutline className="h-6 w-6" />
-                                    <span>Resume</span>
-                                </a>
-                                {isLoading && (
-                                    <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
-                                )}
-                            </button>
-                            <button
-                            onClick={handleDownloadResume2}
-                            className=" text-base hover:text-[#262627] font-medium text-slate-100 cursor-pointer duration-500 ease-in-out bg-icodow hover:bg-inherit shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-2 py-1 rounded-3xl border border-icodow"
-                            >
-                            <a href='/resume-ayush.pdf' target='_blank' className="flex items-center">
-                                <IoEyeOutline className="h-6 w-6" />
-                            </a>
-                            {isLoading2 && (
-                                <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
-                            )}
-                            </button>
-                        </motion.div>
-                       
-                    )}
+                        {isActive && (
+                            <motion.div
+                                key="mobile-menu"
+                                initial={{ opacity: 0, scale: 0.5 }}
+                                animate={{ opacity: 1, scale: 1 }}
+                                exit={{ opacity: 0, scale: 0.3 }}
+                                transition={{ delay: 0.1, type: 'spring' }}
+                                className='p-4 w-275 bg-[#a8b9ee] bg-opacity-70 backdrop-blur-md drop-shadow-lg rounded-lg fixed top-24 right-10 flex flex-col items-center justify-evenly gap-5'>
+                                <Link to="home" spy={true} smooth={true} offset={50} duration={2000} className='text-base  text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Home</Link>
+                                <Link to="about" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> About</Link>
+                                <Link to="timeline" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Timeline</Link>
+                                <Link to="projects" spy={true} smooth={true} offset={0} duration={1500} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Projects</Link>
+                                <Link to="Contact" spy={true} smooth={true} offset={0} duration={1000} className='text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-100 ease-in-out' onClick={() => setIsActive(false)}> Contact me</Link>
+                                <button
+                                    onClick={handleDownloadResume}
+                                    className="text-base text-[#262627] font-medium hover:text-slate-100 cursor-pointer duration-500 ease-in-out hover:bg-icodow shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-3 py-1 rounded-3xl border border-icodow"
+                                >
+                                    <a href='https://drive.google.com/uc?export=download&id=1_cwvSpfDsWjm6ooX9T2EO99zgeigaLrf' className="flex items-center gap-2">
+                                        <IoArrowDownCircleOutline className="h-6 w-6" />
+                                        <span>Resume</span>
+                                    </a>
+                                    {isLoading && (
+                                        <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
+                                    )}
+                                </button>
+                                <button
+                                    onClick={handleDownloadResume2}
+                                    className=" text-base hover:text-[#262627] font-medium text-slate-100 cursor-pointer duration-500 ease-in-out bg-icodow hover:bg-inherit shadow-sm hover:shadow-shadowlue flex items-center gap-2 px-2 py-1 rounded-3xl border border-icodow"
+                                >
+                                    <a href='/resume-ayush.pdf' target='_blank' className="flex items-center">
+                                        <IoEyeOutline className="h-6 w-6" />
+                                    </a>
+                                    {isLoading2 && (
+                                        <div className="border-t-4 border-te animate-spin rounded-full h-6 w-6 ml-2" />
+                                    )}
+                                </button>
+                            </motion.div>
+
+                        )}
                     </AnimatePresence>
                 </div>
             </nav>
         </AnimatePresence>
-        
+
     );
 
 });
