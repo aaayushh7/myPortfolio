e46966e9496da00086c0835f4c9a583ad853f8b2
Ayush Tiwari <101995061+aaayushh7@users.noreply.github.com>
2024-02-08 18:32:51 +0530

    Update time.jsx

--- src/time.jsx
+++ src/time.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
-import 'react-vertical-timeline-component/style.min.css'; 
+import 'react-vertical-timeline-component/style.min.css';
 import { Experience } from './data';
 
 const Time = () => {
